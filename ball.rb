669829d93463c3b21b5b4f3bf0afb29644dde12f
dzyomaArtem <dz.art.23@yandex.ua>
2015-10-19 20:58:33 +0300

    Magic Ball

--- ball.rb
+++ ball.rb
@@ -1,24 +1,7 @@
+require 'yaml'
+
 class Ball
-  ANSWERS = ['Don’t count on it',
-             'My reply is no',
-             'My sources say no',
-             'Outlook not so good',
-             'Very doubtful',
-             'As I see it, yes',
-             'Most likely',
-             'Outlook good',
-             'Signs point to yes',
-             'Yes',
-             'Reply hazy, try again',
-             'Ask again later',
-             'Better not tell you now',
-             'Cannot predict now',
-             'Concentrate and ask again',
-             'It is certain',
-             'It is decidedly so',
-             'Without a doubt',
-             'Yes — definitely',
-             'You may rely on it']
+  ANSWERS = YAML.load_file('answers.yml')
   ARRAYCOL = [31, 32, 33, 34]
 
   def color(rand)
